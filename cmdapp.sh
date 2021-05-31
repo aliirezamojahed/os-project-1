d070104c484a48fa2260ed85f884fffb50fa9ab4
alireza_m8179 <dl7.blogfa@gmail.com>
2021-06-01 02:33:24 +0430

    Cleaning comments and syntax

--- cmdapp.sh
+++ cmdapp.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-for i in $1 $2 $3
+#command line application
+for i in $1 $2 $3  # loop over input parameters
 do
-	if  [ -z $i ] 
+	if  [ -z $i ]  #if one of input params is null, ignore it
 	then
 		continue;
 	fi
 	case $i in
-		"sgi" ) 
-		#system general info.
+		"sgi" )  #system general information
 		echo "system general information";
+		#TODO
 		;;
-		"ssi" )
-		#system security info.
+		"ssi" )  #system security info.
 		echo "system security information";
+		#TODO
 		;;
-		"shi" )
-		#system hardware info.
+		"shi" )  #system hardware info.
 		echo "system hardware information";
+		#TODO
 		;;
-		* )
+		* )  #if param is not any of abow
 		echo "unknown parameter!";
 		;;	
 	esac
