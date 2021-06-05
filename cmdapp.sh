9597076bc3a689196f9b22c8bda30fa1b4aeaabe
Mhdr78 <49515118+Mhdr78@users.noreply.github.com>
2021-06-05 16:56:49 +0430

    Update cmdapp.sh
    Complete SSI part

--- cmdapp.sh
+++ cmdapp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# text colors
+RED='\033[1;31m'
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
 #command line application
 for i in $1 $2 $3  # loop over input parameters
 do
@@ -9,22 +14,32 @@ do
 	fi
 	case $i in
 		"sgi" )  #system general information
-		echo "system general information";
+		echo -e "${CYAN}System General Information${NC}";
 		#TODO
 		;;
 		"ssi" )  #system security info.
-		echo "system security information";
+		echo -e "${CYAN}System Security Information${NC}";
 		#TODO
-		#
-		#
-		#
+		# logged in users
+		liu=$(users) 
+		# active system services
+		ass=$(systemctl --type=service --state=active | more) 
+		# installed applications
+		ip=$(for app in /usr/share/applications/*.desktop ~/.local/share/applications/*.desktop; do app="${app##/*/}"; echo "${app::-8}"; done) 
+		# last system upgrade
+		lsu=$(grep "upgrade " /var/log/dpkg.log | tail -1) 
+
+		echo -e "${RED}Looged In Users:${NC}\n$liu \n"
+		echo -e "${RED}Active System Services:${NC}\n$ass \n"
+		echo -e "${RED}Installed Applications:${NC}\n$ip \n"
+		echo -e "${RED}Last System Upgrade:${NC}\n$lsu \n"
 		;;
 		"shi" )  #system hardware info.
-		echo "system hardware information";
+		echo -e "${CYAN}System Hardware Information${NC}";
 		#TODO
 		;;
 		* )  #if param is not any of abow
-		echo "unknown parameter!";
+		echo -e "${CYAN}unknown parameter!${NC}";
 		;;	
 	esac
 done
