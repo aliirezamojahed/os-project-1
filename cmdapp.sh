c5c86993e5a27c067e29ce1ebfdda24e338b0ff3
Mohammad <dscorpion78@gmail.com>
2021-06-12 09:57:37 -0700

    compelete comments

--- cmdapp.sh
+++ cmdapp.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-#text colors
+# Text colors
 RED='\033[1;31m'
 CYAN='\033[1;36m'
-NC='\033[0m' #no color
+NC='\033[0m' # No color
 
-#command line application
-for i in $1 $2 $3  #loop over input parameters
+# Command line application
+for i in $1 $2 $3  # Loop over input parameters
 do
-	if  [ -n $i ]  #if the input param is not null, then process it 
+	if  [ -n $i ]  # if the input param is not null, then process it 
 	then	
 		case $i in
-			"sgi" )  #system general information
-			echo -e "\n${CYAN}system general information${NC}\n";
-			#overall cpu information
+			"sgi" )  # System General Information
+			echo -e "\n${CYAN}System General Information${NC}\n";
+			# Overall CPU Information
 			oci=$(cat /proc/cpuinfo | head -n 9 | tail -n 8)
-			#name and version of os
+			# Name and version of os
 			nvos=$(hostnamectl | grep "Operating System" | cut -d ':' -f 2)
-			#kernel specification	
+			# Kernel specification	
 			kn=$(uname --kernel-name)
 			kr=$(uname --kernel-release)
 			kv=$(uname --kernel-version)
-			#os distro
+			# OS distro
 			osd=$(cat /etc/*-release | grep "DISTRIB_ID" | cut -d '=' -f 2)
-			#desktop environment 
+			# Desktop environment 
 			de=$(echo $XDG_CURRENT_DESKTOP | cut -d ':' -f 2)
 			if [ "$de" = "GNOME" ]
 			then
@@ -32,65 +32,65 @@ do
 			else
 				gn=0
 			fi
-			#number of active processes 
+			# Number of active processes 
 			nap=$(ps aux --no-headers | wc -l)
-			#15 top processes with highest memory usage
+			# 15 top processes with highest memory usage
 			headers=$(ps aux | head -n 1)
 			hmu=$(ps auxc | sort -rnk 4 | head -15)
-			echo -e "${RED}overall cpu information:\n${NC}$oci"
-		       	echo -e "${RED}name and version of operating system:${NC}$nvos" 
-			echo -e "${RED}kernel specification\n\tname: ${NC}$kn\n\t${RED}release: ${NC}$kr\n\t${RED}version: ${NC}$kv"
-			echo -e "${RED}distro of operating system: ${NC}$osd"
-			echo -e "${RED}desktop environment: ${NC}$de"
+			echo -e "${RED}Overall CPU Information:\n${NC}$oci"
+			echo -e "${RED}Name and Version of Operating System:${NC}$nvos" 
+			echo -e "${RED}Kernel Specification\n\tname: ${NC}$kn\n\t${RED}Release: ${NC}$kr\n\t${RED}Version: ${NC}$kv"
+			echo -e "${RED}Distro of Operating System: ${NC}$osd"
+			echo -e "${RED}Desktop Environment: ${NC}$de"
 			if [ $gn -eq 1 ]
 			then
-				echo -e "\t${RED}gnome version: ${NC}$gv" 
+				echo -e "\t${RED}Gnome Version: ${NC}$gv" 
 			fi
-			echo -e "${RED}number of active processes in this system: ${NC}$nap"
-			echo -e "${RED}15 top processes with highers memory usage:${NC}\n$headers\n$hmu"
+			echo -e "${RED}Number of Active Processes: ${NC}$nap"
+			echo -e "${RED}15 Top processes with highers memory usage:${NC}\n$headers\n$hmu"
 			;;
 	
-			"ssi" )  #system security information
-			echo -e "\n${CYAN}system security information${NC}\n";
-			#logged in users
+			"ssi" )  # System Security Information
+			echo -e "\n${CYAN}System Security Information${NC}\n";
+			# Logged In Users
 			liu=$(users) 
-			#active system services
+			# Active System Services
 			ass=$(systemctl --type=service --state=active) 
-			#installed applications
+			# Installed Applications
 			ip=$(for app in /usr/share/applications/*.desktop ~/.local/share/applications/*.desktop; do app="${app##*/}"; echo "${app::-8}"; done) 
-			#last system upgrade
+			# Last System Upgrade
 			lsu=$(grep "upgrade " /var/log/dpkg.log | tail -1) 
-			echo -e "${RED}looged in users:${NC}\n$liu\n"
-			echo -e "${RED}active system services:${NC}\n$ass\n"
-			echo -e "${RED}installed applications:${NC}\n$ip\n"
-			echo -e "${RED}last system upgrade:${NC}\n$lsu\n"
+			echo -e "${RED}Looged In Users:${NC}\n$liu\n"
+			echo -e "${RED}Active System Services:${NC}\n$ass\n"
+			echo -e "${RED}Installed Applications:${NC}\n$ip\n"
+			echo -e "${RED}Last System Upgrade:${NC}\n$lsu\n"
 			;;
 	
-			"shi" )  #system hardware info.
-			echo -e "\n${CYAN}system hardware information${NC}\n";
-			#cpu hardware details
+			"shi" )  # System Hardware Info.
+			echo -e "\n${CYAN}System Hardware Information${NC}\n";
+			# CPU Hardware Details
 			chd=$(lscpu)
-			#memory hardware details
+			# Memory Hardware Details
 			mhd=$(less /proc/meminfo)
-			#usb devices details
+			# USB Devices Details
 			udd=$(usb-devices)
-			#cpu usage 
+			# CPU Usage 
 			cu=$(ps --no-headers -eo pcpu | awk '{cpu += $1} END {print cpu}')
-			#memory usage 
+			# Memory Usage 
 			mu=$(ps --no-headers -eo pmem | awk '{mem += $1} END {print mem}')
-			#disk usage 
+			# Disk Usage 
 			dus=$(df -h --output=pcent / | tail -n 1)
-			echo -e "${RED}cpu hardware details:${NC}\n$chd\n"
-			echo -e "${RED}memory hardware details:${NC}\n$mhd\n"
-			echo -e "${RED}usb devices details:${NC}\n$udd\n"
-			echo -e "${RED}resources usage"
-			echo -e "${RED}\tcpu usage: ${NC}$cu%"
-			echo -e "${RED}\tmemory usage: ${NC}$mu%"
-			echo -e "${RED}\tdisk usage:${NC}$dus"
+			echo -e "${RED}CPU Hardware Details:${NC}\n$chd\n"
+			echo -e "${RED}Memory Hardware Details:${NC}\n$mhd\n"
+			echo -e "${RED}USB Devices Details:${NC}\n$udd\n"
+			echo -e "${RED}Resources Usage${NC}"
+			echo -e "${RED}\tCPU Usage: ${NC}$cu%"
+			echo -e "${RED}\tMemory Usage: ${NC}$mu%"
+			echo -e "${RED}\tDisk Usage:${NC}$dus"
 			;;
 	
-			* )  #if param is not any of abow
-			echo -e "${CYAN}unknown parameter!${NC}";
+			* )  # if param is not any of abow
+			echo -e "${CYAN}Unknown Parameter!${NC}";
 			;;	
 		esac
 	fi
