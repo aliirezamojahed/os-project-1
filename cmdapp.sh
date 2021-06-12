d1081e914d65c2a9d5bb697f294bac37c923611e
alireza_m8179 <dl7.blogfa@gmail.com>
2021-06-12 04:31:09 +0430

    styling syntax and comments, maybe some more change needed

--- cmdapp.sh
+++ cmdapp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# text colors
+#text colors
 RED='\033[1;31m'
 CYAN='\033[1;36m'
-NC='\033[0m' # No Color
+NC='\033[0m' #no color
 
 #command line application
-for i in $1 $2 $3  # loop over input parameters
+for i in $1 $2 $3  #loop over input parameters
 do
 	if  [ -z $i ]  #if one of input params is null, ignore it
 	then
@@ -14,7 +14,7 @@ do
 	fi
 	case $i in
 		"sgi" )  #system general information
-		echo -e "${CYAN}system general information${NC}";
+		echo -e "\n${CYAN}system general information${NC}\n";
 		#overall cpu information
 		oci=$(cat /proc/cpuinfo | head -n 9 | tail -n 8)
 		#name and version of os
@@ -27,7 +27,7 @@ do
 		osd=$(cat /etc/*-release | grep "DISTRIB_ID" | cut -d '=' -f 2)
 		#desktop environment 
 		de=$(echo $XDG_CURRENT_DESKTOP | cut -d ':' -f 2)
-		if [ $de == "GNOME" ]
+		if [ "$de" = "GNOME" ]
 		then
 			gn=1
 			gv=$(gnome-shell --version | cut -d ' ' -f 3)
@@ -52,30 +52,29 @@ do
 		echo -e "${RED}15 top processes with highers memory usage:${NC}\n$headers\n$hmu"
 		;;
 
-		"ssi" )  #system security info.
-		echo -e "${CYAN}System Security Information${NC}";
-		# logged in users
+		"ssi" )  #system security information
+		echo -e "\n${CYAN}system security information${NC}\n";
+		#logged in users
 		liu=$(users) 
-		# active system services
-		ass=$(systemctl --type=service --state=active | more) 
-		# installed applications
+		#active system services
+		ass=$(systemctl --type=service --state=active) 
+		#installed applications
 		ip=$(for app in /usr/share/applications/*.desktop ~/.local/share/applications/*.desktop; do app="${app##/*/}"; echo "${app::-8}"; done) 
-		# last system upgrade
+		#last system upgrade
 		lsu=$(grep "upgrade " /var/log/dpkg.log | tail -1) 
-
-		echo -e "${RED}Looged In Users:${NC}\n$liu \n"
-		echo -e "${RED}Active System Services:${NC}\n$ass \n"
-		echo -e "${RED}Installed Applications:${NC}\n$ip \n"
-		echo -e "${RED}Last System Upgrade:${NC}\n$lsu \n"
+		echo -e "${RED}looged in users:${NC}\n$liu \n"
+		echo -e "${RED}active system services:${NC}\n$ass \n"
+		echo -e "${RED}installed applications:${NC}\n$ip \n"
+		echo -e "${RED}last system upgrade:${NC}\n$lsu \n"
 		;;
 
 		"shi" )  #system hardware info.
-		echo -e "${CYAN}System Hardware Information${NC}";
-		# CPU hardware details
+		echo -e "\n${CYAN}system hardware information${NC}\n";
+		#cpu hardware details
 		chd=$(lscpu)
-		# Memory hardware details
+		#memory hardware details
 		mhd=$(less /proc/meminfo)
-		# USB devices details
+		#usb devices details
 		udd=$(usb-devices)
 		#cpu usage 
 		cu=$(ps --no-headers -eo pcpu | awk '{cpu += $1} END {print cpu}')
@@ -83,12 +82,13 @@ do
 		mu=$(ps --no-headers -eo pmem | awk '{mem += $1} END {print mem}')
 		#disk usage 
 		dus=$(df -h --output=pcent / | tail -n 1)
-		echo -e "${RED}CPU Hardware Details:${NC}\n$chd \n"
-		echo -e "${RED}Memory Hardware Details:${NC}\n$mhd \n"
-		echo -e "${RED}USB devices details:${NC}\n$udd \n"
-		echo -e "${RED}cpu usage: ${NC}$cu%"
-		echo -e "${RED}memory usage: ${NC}$mu%"
-		echo -e "${RED}disk usage:${NC}$dus"
+		echo -e "${RED}cpu hardware details:${NC}\n$chd \n"
+		echo -e "${RED}memory hardware details:${NC}\n$mhd \n"
+		echo -e "${RED}usb devices details:${NC}\n$udd \n"
+		echo -e "${RED}resources usage"
+		echo -e "${RED}\tcpu usage: ${NC}$cu%"
+		echo -e "${RED}\tmemory usage: ${NC}$mu%"
+		echo -e "${RED}\tdisk usage:${NC}$dus"
 		;;
 
 		* )  #if param is not any of abow
